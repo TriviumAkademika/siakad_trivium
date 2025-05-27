5230a4c06257acb5c5f053030b53f597e4f0d8bc
rsyidatr <rasyidaturrahma@gmail.com>
2025-05-27 09:23:24 +0700

    fix: sidebar nilai(remove admin)

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -132,7 +132,7 @@
             @endif
 
             {{-- Nilai tampilan dosen --}}
-            @if(auth()->user()->role === 'dosen' || auth()->user()->role === 'admin')
+            @if(auth()->user()->role === 'dosen')
                 <a href="{{ route('nilai-dosen') }}" class="flex items-center gap-2 {{ request()->routeIs('nilai-dosen') ? 'bg-brand-200' : '' }} p-2 rounded-lg">
                     <i class="ph ph-ranking text-xl text-hitam"></i>
                     <span class="text-base text-hitam">Nilai</span>
